b3600359d6681f2e81c8630d0dc3eed24dc3dc59
Vinicius <vteixeira212@gmail.com>
2023-11-04 13:39:08 -0300

    chore: create delete, put and patch

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Post, Get } from "@nestjs/common";
-import { Param } from "@nestjs/common/decorators/http/route-params.decorator";
+import { Body, Controller, Post, Get, Patch, Put, Param, Delete } from "@nestjs/common";
 
 @Controller('users')
 export class UserController{
@@ -15,8 +14,33 @@ export class UserController{
     }
 
     @Get(':id')
-    async readOne(@Param() param){
-      return {user:{}}
+    async show(@Param() param){
+      return {user:{}, param}
+    }
+
+    @Put()
+    async update(@Body() body, @Param() param){
+      return {
+        method: 'put',
+        body,
+        param
+      }
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() body, @Param() param){
+      return {
+        method: 'patch',
+        body,
+        param
+      }
+    }
+
+    @Delete(':id')
+    async delete(@Param() param){
+      return {
+        param
+      }
     }
 
 }
