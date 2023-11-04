99fbdff3304719ed624149aa82a055701f519bd0
Vinicius <vteixeira212@gmail.com>
2023-11-04 13:33:43 -0300

    chore: create get methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,22 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Get } from "@nestjs/common";
+import { Param } from "@nestjs/common/decorators/http/route-params.decorator";
 
 @Controller('users')
 export class UserController{
 
     @Post()
-    setHello(@Body() body): string {
+    async setHello(@Body() body) {
       return body;
+    }
+
+    @Get()
+    async list(){
+      return {users:[]}
+    }
+
+    @Get(':id')
+    async readOne(@Param() param){
+      return {user:{}}
     }
 
 }
